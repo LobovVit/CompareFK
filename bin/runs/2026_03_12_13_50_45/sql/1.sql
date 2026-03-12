f6f368b2e65c80623e85772416cb32e766320857
lobovvitaliy <lobov.vitaliy@gmail.com>
2026-03-12 14:14:07 +0300

    v 2_03-pass

--- bin/runs/2026_03_12_13_50_45/sql/1.sql
+++
@@ -1,3 +0,0 @@
-select id
-from bra.doc_d_001
-         limit 1000;
--- bin/runs/2026_03_12_13_50_45/sql/2.sql
+++
@@ -1,7 +0,0 @@
-select dd.id
-from bra.doc_d_001 dd
-         cross join lateral (
-    select pg_sleep(0.02)
-where dd.id is not null
-    ) s
-limit 400;
